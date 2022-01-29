903b6da5fea6adf0488365cb34ca31fa11c61718
Zahra Shahid <zashahid45@gmail.com>
2022-01-29 19:03:42 +0500

    node modules added and footer edited

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,14 +11,6 @@ const Footer = (LightTheme) => {
       }}/>
       <span className="name">
         Made by{" Zahra Shahid "}
-        <a href="https://github.com/ZahraShahid" target="__blank" text-decoration="none" 
-         style={{
-            backgroundColor: LightTheme ? "#3b5360" : "white",
-            color: LightTheme ? "white" : "black",
-          }}
-        >
-           Find out more..
-        </a>
       </span>
     </div>
   );
